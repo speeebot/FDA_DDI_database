5ffe683b2947710c95658b46799d330dbf7e7ffd
Matias <64340009+CineraM@users.noreply.github.com>
2023-11-11 22:26:03 -0500

    Webapp basic routing

--- testing_scripts/test.py
+++ testing_scripts/test.py
@@ -6,11 +6,7 @@ url = 'https://api.fda.gov/drug/drugsfda.json?limit=1'
 
 # https://open.fda.gov/apis/drug/drugsfda/example-api-queries/
 
-
 request = requests.get(url)
 
 my_dict = dict(request.json())
 print(my_dict)
-
-
-
