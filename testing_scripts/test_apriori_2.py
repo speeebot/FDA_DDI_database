01da3d63c302177729ea900ea80172fc4e7676fb
BiancaDeHaan <48565983+BiancaDeHaan@users.noreply.github.com>
2023-11-19 17:21:37 -0500

    Update test_apriori_2.py

--- testing_scripts/test_apriori_2.py
+++ testing_scripts/test_apriori_2.py
@@ -10,16 +10,17 @@ OPENFDA_API_ENDPOINT = 'https://api.fda.gov/drug/event.json'
 SEARCH_AFTER_FILENAME = 'FDA_DDI_database/data/search_after_token.json'
 DATA_FILENAME = 'FDA_DDI_database/data/fda_data.json'
 DRUG_EVENT_FIELDS = ['patient.drug.medicinalproduct', 'patient.reaction.reactionmeddrapt']
+SYMPTOM = "Acute kidney injury"
 
 def fetch_adverse_events_with_aki(drug_name):
     # Parameters
     results = []
     skip = 0  # Used for pagination
-    total_limit = 5000  # Set this to None or a large number if you want to get all records.
+    total_limit = 1000  # Set this to None or a large number if you want to get all records.
     limit_per_request = 1000  # The maximum allowed by openFDA per request is 1000.
 
     # Define the search query to include AKI
-    search_query = f'patient.drug.medicinalproduct:"{drug_name}" AND patient.reaction.reactionmeddrapt.exact:"acute kidney injury"'
+    search_query = f'patient.drug.medicinalproduct:"{drug_name}"'
 
     while True:
         params = {
@@ -53,13 +54,20 @@ data = fetch_adverse_events_with_aki(DRUG_OF_INTEREST)
 drug_data = []
 for event in data:
     patient = event['patient']
-    info = patient['drug']
+    drug = patient['drug']
+    reaction = patient['reaction']
     drugs = []
-    for drug in info:
-        drugs.append(drug['medicinalproduct'])
+    for d in drug:
+        drugs.append(d['medicinalproduct'])
+    for r in reaction:
+        # print(r)
+        name = r['reactionmeddrapt']
+        if name==SYMPTOM:
+            drugs.append(name)
+    
     drug_data.append(drugs)
 
-
+print(drug_data)
 
 # Print out some information on the original data
 # print("LOG: Columns in data\n", data.columns)
@@ -69,20 +77,21 @@ for event in data:
 # Only using the 'Suspect Product Names' row
 
 # Remove rows that don't have the target drug - greatly reduces time spent in algorithm if done in preprocessing
+'''
 print("Rows before filtering target drug:", len(drug_data))
 filtered_data = [drug_list for drug_list in drug_data if DRUG_OF_INTEREST in drug_list]
 print("Rows after filtering target drug:", len(filtered_data))
-
+'''
 # Encoder is used by Apriori
 te = TransactionEncoder()
-te_ary = te.fit(filtered_data).transform(filtered_data)
+te_ary = te.fit(drug_data).transform(drug_data)
 df_apriori = pd.DataFrame(te_ary, columns=te.columns_)
 # This prints out the table neatly showing True/False for every row in the original table, and the drugs
-# print(df_apriori)
+print(df_apriori)
 
 # Apply Apriori Algorithm
 # We can modify min_support: the lower it is, the more results we get, however they might be less reliable
-frequent_itemsets = apriori(df_apriori, min_support=0.05, use_colnames=True)
+frequent_itemsets = apriori(df_apriori, min_support=0.01, use_colnames=True)
 
 # Collecting the inferred rules in a dataframe 
 rules = association_rules(frequent_itemsets, metric ="lift", min_threshold = 1) 
@@ -92,5 +101,5 @@ rules = rules.sort_values(['lift'], ascending =[False])
 pd.set_option('display.max_rows', None)
 print(rules) 
 
-filtered_rules = rules[(rules['antecedents'] == {(DRUG_OF_INTEREST)})]
+filtered_rules = rules[(rules['consequents'] == {("Acute kidney injury")})]
 print(filtered_rules)
