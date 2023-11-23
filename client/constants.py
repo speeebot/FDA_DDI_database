613afda2a4f6f3e52c0632ff4874153438c44b86
Matias <64340009+CineraM@users.noreply.github.com>
2023-11-23 17:56:44 -0500

    filtering data, displaying in webapp

---
+++ client/constants.py
@@ -0,0 +1,7 @@
+# Constants
+DRUG_OF_INTEREST = 'Vancomycin'
+ADVERSE_REACTION = 'acute kidney injury'
+OPENFDA_API_ENDPOINT = 'https://api.fda.gov/drug/event.json'
+SEARCH_AFTER_FILENAME = 'GitHub/FDA_DDI_database/client/search_after_token.json'
+DATA_FILENAME = 'GitHub/FDA_DDI_database/client/fda_data.json'
+DRUG_EVENT_FIELDS = ['patient.drug.medicinalproduct', 'patient.reaction.reactionmeddrapt']
